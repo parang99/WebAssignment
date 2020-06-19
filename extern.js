6c9097495bb369c649643de371fff344310c65e1
parang99 <53161476+parang99@users.noreply.github.com>
2020-06-20 00:43:20 +0900

    Add files via upload

--- extern.js
+++ extern.js
@@ -5,60 +5,43 @@ function idDuplicateVerify(){
   document.getElementById("user_id_temp").value = temp;
 }
 
-// order0.html
-var fruit1 = new Array("fruit11", "fruit12", "fruit13", "fruit14", "fruit15", "fruit16", "fruit17", "fruit18");
-var fruit2 = new Array("fruit21", "fruit22", "fruit23", "fruit24", "fruit25", "fruit26", "fruit27", "fruit28");
-var fruit3 = new Array("fruit31", "fruit32", "fruit33", "fruit34", "fruit35", "fruit36", "fruit37");
-var fruit4 = new Array("fruit41", "fruit42", "fruit43", "fruit44", "fruit45", "fruit46", "fruit47", "fruit48", "fruit49");
+// font - order0.html
+var orderFont = 0;
+function orderFontLarge(button){
+  var tab = document.getElementById("mytable")
+  var btn = document.getElementById(button);
+  if (orderFont == 0){
+    tab.style.fontSize = "20pt";
+    btn.value = "글씨 원래대로";
+    orderFont = 1;
+  }
+  else {
+    tab.style.fontSize = "12pt";
+    btn.value = "글씨 크게";
+    orderFont = 0;
+  }
+}
 
-var price1 = new Array(9480, 12000, 12000, 9900, 28900, 7980, 12900, 15000);
-var price2 = new Array(30000, 2890, 7980, 10000, 15900, 8980, 12900, 5480);
-var price3 = new Array(2300, 4900, 7980, 5800, 5900, 1750, 3500);
-var price4 = new Array(6480, 5480, 4180, 1990, 6780, 11900, 8800, 29000, 10000);
+// calculate - order0.html
+var fruit = new Array("fruit11", "fruit12", "fruit13", "fruit14", "fruit15", "fruit16", "fruit17", "fruit18"
+                      , "fruit21", "fruit22", "fruit23", "fruit24", "fruit25", "fruit26", "fruit27", "fruit28"
+                      , "fruit31", "fruit32", "fruit33", "fruit34", "fruit35", "fruit36", "fruit37"
+                      , "fruit41", "fruit42", "fruit43", "fruit44", "fruit45", "fruit46", "fruit47", "fruit48", "fruit49");
+
+var price = new Array(9480, 12000, 12000, 9900, 28900, 7980, 12900, 15000
+                      , 30000, 2890, 7980, 10000, 15900, 8980, 12900, 5480
+                      , 2300, 4900, 7980, 5800, 5900, 1750, 3500
+                      , 6480, 5480, 4180, 1990, 6780, 11900, 8800, 29000, 10000);
 
 var shipping = [3000, 2500, 2500, 4000];
 
-// order1.html
-function order1Function(){
+function orderFunction(num, start, ship, name){
   var sum = 0;
   var n;
-  for(i = 0;i < 8;i++){
-    n = document.getElementById(fruit1[i]).value;
-    sum = sum + price1[i] * n;
+  for(i = start;i < start + num;i++){
+    n = document.getElementById(fruit[i]).value;
+    sum = sum + price[i] * n;
   }
-  sum += shipping[0];
-  alert("[눈송과수원] 총 " + sum + "원(배송비 포함)의 주문이 완료되었습니다");
-}
-// order2.html
-function order2Function(){
-  var sum = 0;
-  var n;
-  for(i = 0;i < 8;i++){
-    n = document.getElementById(fruit2[i]).value;
-    sum = sum + price2[i] * n;
-  }
-  sum += shipping[1];
-  alert("[동네과일트럭] 총 " + sum + "원(배송비 포함)의 주문이 완료되었습니다");
-}
-// order3.html
-function order3Function(){
-  var sum = 0;
-  var n;
-  for(i = 0;i < 7;i++){
-    n = document.getElementById(fruit3[i]).value;
-    sum = sum + price3[i] * n;
-  }
-  sum += shipping[2];
-  alert("[숙명건조공장] 총 " + sum + "원(배송비 포함)의 주문이 완료되었습니다");
-}
-// order4.html
-function order4Function(){
-  var sum = 0;
-  var n;
-  for(i = 0;i < 9;i++){
-    n = document.getElementById(fruit4[i]).value;
-    sum = sum + price4[i] * n;
-  }
-  sum += shipping[3];
-  alert("[세계과일수입] 총 " + sum + "원(배송비 포함)의 주문이 완료되었습니다");
+  sum += shipping[ship];
+  alert("[" + name + "] 총 " + sum + "원(배송비 포함)의 주문이 완료되었습니다");
 }
